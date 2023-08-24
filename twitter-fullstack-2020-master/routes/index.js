1a5af8a1e1593f98d80a70c40e1f778ae0187767
user <carola1408@yahoo.com.tw>
2023-08-24 16:13:41 +0800

    feat:add user signup page

--- twitter-fullstack-2020-master/routes/index.js
+++ twitter-fullstack-2020-master/routes/index.js
@@ -1,14 +1,21 @@
+// Controllers
 const express = require('express')
 const router = express.Router()
 const tweetController = require('../controllers/tweet-controller')
-
+const userController = require('../controllers/user-controller')
 router.get('/tweets', tweetController.getTweets)
 
 router.use('/', (req, res) => res.redirect('/tweets'))
 
+// Admin
 const admin = require('./modules/admin')
 router.use('/admin', admin)
 
+// Sign up
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+
+// Tweets
 router.get('/tweets', tweetController.getTweets)
 
 router.use('/', (req, res) => res.redirect('/tweets'))
