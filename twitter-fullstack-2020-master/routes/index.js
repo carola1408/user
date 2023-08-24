cdd43fe91034e1421e6454d14b28868b11241c1c
user <carola1408@yahoo.com.tw>
2023-08-24 15:57:24 +0800

    feat:add admin index page

---
+++ twitter-fullstack-2020-master/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const tweetController = require('../controllers/tweet-controller')
+
+router.get('/tweets', tweetController.getTweets)
+
+router.use('/', (req, res) => res.redirect('/tweets'))
+
+const admin = require('./modules/admin')
+router.use('/admin', admin)
+
+router.get('/tweets', tweetController.getTweets)
+
+router.use('/', (req, res) => res.redirect('/tweets'))
+module.exports = router
